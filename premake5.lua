1b0099466720c5a54531ddde224c57b3aaf58198
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2023-05-01 15:47:00 -0700

    Update premake5.lua
    Fixed formatting and updated referenced system version.

--- premake5.lua
+++ premake5.lua
@@ -2,11 +2,10 @@ project "GLFW"
     kind "StaticLib"
     language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+    files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
@@ -18,28 +17,26 @@ project "GLFW"
         "src/window.c"
     }
 
-	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
-        staticruntime "On"
+    filter "system:windows"
+    buildoptions { "-std=c11", "-lgdi32" }
+    systemversion "latest"
+    staticruntime "On"
 
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
+    files {
+        "src/win32_init.c",
+        "src/win32_joystick.c",
+        "src/win32_monitor.c",
+        "src/win32_time.c",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
+    }
 
-		defines 
-		{ 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-		}
+    defines { 
+        "_GLFW_WIN32",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
