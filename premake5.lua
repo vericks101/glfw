7319a666d7ab4a82aa1695def382cc7a6ea5a0d0
Victor Erickson <vericks101@gmail.com>
2023-05-11 15:53:54 -0700

    Modified to build as a static library and cleand up premake file.

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,41 +24,13 @@ project "GLFW"
         "src/null_platform.h",
         "src/null_joystick.h",
         "src/null_init.c",
-
         "src/null_monitor.c",
         "src/null_window.c",
         "src/null_joystick.c",
     }
-    filter "system:linux"
-        pic "On"
-
-        systemversion "latest"
-        staticruntime "On"
-
-        files {
-            "src/x11_init.c",
-            "src/x11_monitor.c",
-            "src/x11_window.c",
-            "src/xkb_unicode.c",
-            "src/posix_time.c",
-            "src/posix_thread.c",
-            "src/glx_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c",
-            "src/linux_joystick.c"
-        }
-
-        defines {
-            "_GLFW_X11"
-        }
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
-    
-        -- buildoptions{
-        --     "/MT"
-        -- }
 
         files {
             "src/win32_init.c",
@@ -80,9 +53,9 @@ project "GLFW"
         }
 
     filter "configurations:Debug"
-        runtime "Debug"
-        symbols "On"
+		runtime "Debug"
+		symbols "on"
 
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "On"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
